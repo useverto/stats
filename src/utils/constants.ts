d001ffff68cd12850a42ec78889ad8d4cdcc4822
John Letey <johnletey@gmail.com>
2020-09-15 16:46:54 +0100

    feat: Pull price data

---
+++ src/utils/constants.ts
@@ -0,0 +1,1 @@
+export const exchangeWallet = "aLemOhg9OGovn-0o4cOCbueiHT9VgdYnpJpq7NgMA1A";
---
+++ src/utils/gql.ts
@@ -0,0 +1,15 @@
+export async function query({ query, variables = null }) {
+  const graphql = JSON.stringify({
+    query,
+    variables,
+  });
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: graphql,
+  };
+  const res = await fetch("https://arweave.dev/graphql", requestOptions);
+  return await res.clone().json();
+}
