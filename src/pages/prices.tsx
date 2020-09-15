f5ae66d645d951de38e161ea7c9ec91cabd49c2d
John Letey <johnletey@gmail.com>
2020-09-15 16:32:12 +0100

    feat: Grab the token from the URL

--- src/pages/prices.tsx
+++ src/pages/prices.tsx
@@ -1,11 +1,12 @@
-const Prices = ({}) => {
-  return <h1>Nothing yet ...</h1>;
-};
+import { useRouter } from "next/router";
 
-export const getStaticProps = () => {
-  return {
-    props: {},
-  };
+const Prices = () => {
+  const router = useRouter();
+  const token = router.query.token;
+
+  if (!token || token === "") return <p>no token.</p>;
+
+  return <p>nothing yet ...</p>;
 };
 
 export default Prices;
