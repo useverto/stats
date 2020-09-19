b7184c73238735822789ded91416bac99c083e61
John Letey <johnletey@gmail.com>
2020-09-19 19:58:32 +0100

    feat: switch over to `@verto/lib`

--- src/pages/price.tsx
+++ src/pages/price.tsx
@@ -1,122 +1,7 @@
-import { getTradingPosts } from "../utils/get_trading_posts";
-import { query } from "../utils/gql";
-import moment from "moment";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import Verto from "@verto/lib";
 import { Line } from "react-chartjs-2";
-
-const grabPrices = async (token) => {
-  const posts = await getTradingPosts();
-
-  const maxInt = 2147483647;
-
-  const orderTxs = (
-    await query({
-      query: `
-    query($recipients: [String!]) {
-      transactions(
-        recipients: $recipients
-        tags: [
-          { name: "Exchange", values: "Verto" }
-          { name: "Type", values: "Buy" }
-          { name: "Token", values: "${token}" }
-        ]
-        first: ${maxInt}
-      ) {
-        edges {
-          node {
-            id
-            block {
-              timestamp
-            }
-            quantity {
-              ar
-            }
-          }
-        }
-      }
-    }`,
-      variables: {
-        recipients: posts,
-      },
-    })
-  ).data.transactions.edges;
-
-  let orders: { id: string; amnt: number; timestamp: number }[] = [];
-  orderTxs.map((order) => {
-    orders.push({
-      id: order.node.id,
-      amnt: parseFloat(order.node.quantity.ar),
-      timestamp: order.node.block.timestamp,
-    });
-  });
-
-  let prices: number[] = [];
-  let days: string[] = [];
-
-  let high = moment().add(1, "days").hours(0).minutes(0).seconds(0);
-  const limit =
-    orders[orders.length - 1].timestamp >= 1599955200
-      ? orders[orders.length - 1].timestamp
-      : 1599955200;
-  while (high.unix() >= limit) {
-    let dayPrices: number[] = [];
-
-    const low = high.clone().subtract(1, "days");
-    for (const order of orders) {
-      if (order.timestamp <= high.unix() && order.timestamp >= low.unix()) {
-        const confirmationTx = (
-          await query({
-            query: `
-          query($txID: [String!]!) {
-            transactions(
-              tags: [
-                { name: "Exchange", values: "Verto" }
-                { name: "Type", values: "Confirmation" }
-                { name: "Match", values: $txID }
-              ]
-            ) {
-              edges {
-                node {
-                  tags {
-                    name
-                    value
-                  }
-                }
-              }
-            }
-          }`,
-            variables: {
-              txID: order.id,
-            },
-          })
-        ).data.transactions.edges;
-
-        if (confirmationTx.length === 1) {
-          dayPrices.push(
-            order.amnt /
-              parseFloat(
-                confirmationTx[0].node.tags
-                  .find((tag) => tag.name === "Received")
-                  .value.split(" ")[0]
-              )
-          );
-        }
-      }
-    }
-
-    if (dayPrices.length === 0) {
-      prices.push(prices[prices.length - 1]);
-    } else {
-      prices.push(dayPrices.reduce((a, b) => a + b, 0) / dayPrices.length);
-    }
-    days.push(low.format("MMM DD"));
-
-    high = low;
-  }
-
-  return { prices: prices.reverse(), dates: days.reverse() };
-};
 
 const Price = () => {
   const router = useRouter();
@@ -124,7 +9,7 @@ const Price = () => {
 
   if (!token || token === "") return <p>no token.</p>;
 
-  const { data, error } = useSWR([token], grabPrices);
+  const { data, error } = useSWR([token], new Verto().price);
 
   if (error) return <p>failed to load.</p>;
   // @ts-ignore
--- src/pages/volume.tsx
+++ src/pages/volume.tsx
@@ -1,81 +1,7 @@
-import { getTradingPosts } from "../utils/get_trading_posts";
-import { query } from "../utils/gql";
-import moment from "moment";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import Verto from "@verto/lib";
 import { Line } from "react-chartjs-2";
-
-const grabVolume = async (token) => {
-  const posts = await getTradingPosts();
-
-  const maxInt = 2147483647;
-
-  const orderTxs = (
-    await query({
-      query: `
-    query($recipients: [String!]) {
-      transactions(
-        recipients: $recipients
-        tags: [
-          { name: "Exchange", values: "Verto" }
-          { name: "Type", values: "Sell" }
-          { name: "Contract", values: "${token}" }
-        ]
-        first: ${maxInt}
-      ) {
-        edges {
-          node {
-            block {
-              timestamp
-            }
-            tags {
-              name
-              value
-            }
-          }
-        }
-      }
-    }`,
-      variables: {
-        recipients: posts,
-      },
-    })
-  ).data.transactions.edges;
-
-  let orders: { amnt: number; timestamp: number }[] = [];
-  orderTxs.map((order) => {
-    orders.push({
-      amnt: JSON.parse(
-        order.node.tags.find((tag) => tag.name === "Input").value
-      ).qty,
-      timestamp: order.node.block.timestamp,
-    });
-  });
-
-  let volume: number[] = [];
-  let days: string[] = [];
-
-  if (orders.length > 0) {
-    let high = moment().add(1, "days").hours(0).minutes(0).seconds(0);
-    while (high.unix() >= orders[orders.length - 1].timestamp) {
-      let sum = 0;
-
-      const low = high.clone().subtract(1, "days");
-      orders.map((order) => {
-        if (order.timestamp <= high.unix() && order.timestamp >= low.unix()) {
-          sum += order.amnt;
-        }
-      });
-
-      volume.push(sum);
-      days.push(low.format("MMM DD"));
-
-      high = low;
-    }
-  }
-
-  return { volume: volume.reverse(), dates: days.reverse() };
-};
 
 const Volume = () => {
   const router = useRouter();
@@ -83,7 +9,7 @@ const Volume = () => {
 
   if (!token || token === "") return <p>no token.</p>;
 
-  const { data, error } = useSWR([token], grabVolume);
+  const { data, error } = useSWR([token], new Verto().volume);
 
   if (error) return <p>failed to load.</p>;
   // @ts-ignore
